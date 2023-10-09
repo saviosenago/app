2b72ef3e566db695c0d6eb077c8a566cc0146b61
Savio Sena <saviogoferreira@gmail.com>
2023-10-09 11:48:44 -0300

    y

--- App.js
+++ App.js
@@ -1,20 +1,136 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  ImageBackground,
+  Image,
+  StyleSheet,
+} from 'react-native';
 
-export default function App() {
+function LoginScreen() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {};
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ImageBackground
+      source={require('./assets/chain.png')}
+      style={styles.backgroundImage}
+    >
+      <View style={styles.container}>
+        <Image source={require('./assets/icon.png')} style={styles.iconImage} />
+        <Text style={styles.title}>Loja de Mangás</Text>
+        <Text style={styles.label}>Nome de Usuário</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Digite seu nome de usuário"
+          value={username}
+          onChangeText={setUsername}
+        />
+        <Text style={styles.label}>Senha</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Digite sua senha"
+          secureTextEntry
+          value={password}
+          onChangeText={setPassword}
+        />
+
+        <TouchableOpacity style={styles.forgotPasswordButton}>
+          <Text style={styles.forgotPasswordText}>Esqueci Senha</Text>
+        </TouchableOpacity>
+
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.button} onPress={handleLogin}>
+            <Text style={styles.buttonText}>Login</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.signupButton}>
+            <Text style={styles.signupText}>Cadastro</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  backgroundImage: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    resizeMode: 'cover',
     justifyContent: 'center',
   },
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 16,
+    backgroundColor: 'rgba(255, 255, 255, 0.8)',
+  },
+  iconImage: {
+    width: 200,
+    height: 200,
+    marginBottom: 16,
+  },
+  title: {
+    fontSize: 40,
+    fontWeight: 'bold',
+    marginBottom: 16,
+    color: 'red',
+    fontFamily: 'AvenirNextCondensed-MediumItalic',
+  },
+  label: {
+    marginBottom: 8,
+  },
+  input: {
+    width: '100%',
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 20,
+    paddingHorizontal: 10,
+    marginBottom: 16,
+    backgroundColor: 'white',
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '60%',
+    marginTop: 16,
+  },
+  button: {
+    backgroundColor: 'red',
+    paddingVertical: 12,
+    paddingHorizontal: 24,
+    borderRadius: 7,
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  forgotPasswordButton: {
+    marginTop: 1,
+  },
+  forgotPasswordText: {
+    color: 'blue',
+    textDecorationLine: 'underline',
+    right: '34%',
+  },
+  signupButton: {
+    backgroundColor: 'red',
+    paddingVertical: 12,
+    paddingHorizontal: 14,
+    borderRadius: 7,
+    textAlign: 'center',
+  },
+  signupText: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
 });
+
+export default LoginScreen;
